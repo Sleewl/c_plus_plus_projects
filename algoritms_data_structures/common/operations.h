7bcf2ceb6376c830f155f899c91aee089a3897a2
Sleewl <163043842+Sleewl@users.noreply.github.com>
2025-04-21 15:37:22 +0300

    Update operations.h

--- algoritms_data_structures/common/operations.h
+++ algoritms_data_structures/common/operations.h
@@ -1,7 +1,7 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
-namespace bazhenov {
+namespace gruzdev {
   long long int summarize(long long int left, long long int right);
   long long int subtract(long long int left, long long int right);
   long long int multiply(long long int left, long long int right);
