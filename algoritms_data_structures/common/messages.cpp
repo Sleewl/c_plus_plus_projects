826f822173958e518a3c735c638b92c3434a814a
Sleewl <163043842+Sleewl@users.noreply.github.com>
2025-04-21 15:36:48 +0300

    Update messages.cpp

--- algoritms_data_structures/common/messages.cpp
+++ algoritms_data_structures/common/messages.cpp
@@ -1,22 +1,22 @@
 #include "messages.h"
 #include <iostream>
 
-std::ostream& bazhenov::printInvalidCommand(std::ostream& out)
+std::ostream& gruzdev::printInvalidCommand(std::ostream& out)
 {
   return out << "<INVALID COMMAND>";
 }
 
-std::ostream& bazhenov::printEmpty(std::ostream& out)
+std::ostream& gruzdev::printEmpty(std::ostream& out)
 {
   return out << "<EMPTY>";
 }
 
-std::ostream& bazhenov::printTrue(std::ostream& out)
+std::ostream& gruzdev::printTrue(std::ostream& out)
 {
   return out << "<TRUE>";
 }
 
-std::ostream& bazhenov::printFalse(std::ostream& out)
+std::ostream& gruzdev::printFalse(std::ostream& out)
 {
   return out << "<FALSE>";
 }
