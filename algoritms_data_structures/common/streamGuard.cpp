d8e58d28b6bfa556a056aabceb9806c1ee298efd
Sleewl <163043842+Sleewl@users.noreply.github.com>
2025-04-21 15:38:14 +0300

    Update streamGuard.cpp

--- algoritms_data_structures/common/streamGuard.cpp
+++ algoritms_data_structures/common/streamGuard.cpp
@@ -1,12 +1,12 @@
 #include "streamGuard.hpp"
 
-bazhenov::StreamGuard::StreamGuard(std::ios& s):
+gruzdev::StreamGuard::StreamGuard(std::ios& s):
   s_(s),
   precision_(s.precision()),
   flags_(s.flags())
 {}
 
-bazhenov::StreamGuard::~StreamGuard()
+gruzdev::StreamGuard::~StreamGuard()
 {
   s_.precision(precision_);
   s_.flags(flags_);
